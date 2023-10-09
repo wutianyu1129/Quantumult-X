4f835d5d8761448111113e969bdeb78d5a87c53e
吴天宇 <86960011+wutianyu1129@users.noreply.github.com>
2023-10-10 01:18:02 +0800

    Update cc98.js

--- cc98.js
+++ cc98.js
@@ -1,12 +1,3 @@
-function hideAds(response, url, header) {
-    var body = response.body;
-    // 使用正则表达式匹配广告位的HTML结构
-    var adRegex = /<div style="position: relative; width: 18.75rem; height: 6.25rem;">[\s\S]*?<\/div>/g;
-    body = body.replace(adRegex, ""); // 将广告位的HTML结构替换为空，实现隐藏广告
-
-    // 更新响应的内容
-    response.body = body;
-    $done(response);
-}
-
-$done({ response: $response });
+let body = $response.body;
+body = body.replace(/<div class="topicInfo-ads">[\s\S]*?<\/div>/g, "");
+$done({ body });
